278f21df28a8c55662222e5e8d1209ccb7c52427
LexFTW <lexmengual@gmail.com>
2019-02-14 20:12:03 +0100

    First Commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+const users = [
+  {name : 'alexis.mengual', password : 'Contraseña'},
+  {name : 'duson', password : '1234'},
+  {name : 'emieza', password : '1234'}
+]
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res){
+  for (var i = 0; i < users.length; i++) {
+    if(users[i]['name'] == req.body.username && users[i]['password'] == req.body.password){
+      res.render('dashboard', {username : req.body.username});
+      return;
+    }
+  }
+});
+
+router.get('/api/login/:user/:pass', function(req, res){
+  for (var i = 0; i < users.length; i++) {
+    if(users[i]['name'] == req.params.user && users[i]['password'] == req.params.pass){
+      var msg = {"status" : "OK"};
+      res.send(JSON.stringify(msg));
+      return;
+    }
+  }
+  var msg = {"status" : "ERROR"};
+  res.send(JSON.stringify(msg));
+});
+
+module.exports = router;
